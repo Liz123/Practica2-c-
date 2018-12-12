4deca7b7de94134c4c2577fb00192ceda3f595e9
Liz123 <joanaReyna4@gmail.com>
2018-12-12 03:15:54 -0600

    Add files via upload

---
+++ practica1.cpp
@@ -0,0 +1,29 @@
+//Comentarios 
+//Libreria principal
+#include<iostream>
+
+using namespace std
+
+void namespace std;
+
+void main()
+{
+	//Datos 
+	int horasTrabajadas;
+	float costoHora;
+	float sueldo;
+	<< salida de mensajes
+	>> Guarda alguna variable (introducido)
+	//<<Salida de mensajes 
+	cout <<"INGRESE HRS TRABAJAS POR EL OPERACION";
+	// el(>>)A guarda el dato en la variable horasTrabajadas
+	cin >>horasTrabajas;
+	cout <<"INGRESE EL PAGO POR HORA";
+	cin >>costoHora;
+	sueldo = horasTrabajadas * costoHora;
+    cout <<"EL SUELDO TOTAL:";
+    cout <<sueldo;
+    //cin es para obtener el dato de la variable y el get() es un metodo que obtener el valor;
+    cin.get();
+    cin.get();
+}
